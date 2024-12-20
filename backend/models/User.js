40f318f9aa9d29e47ab6c319eebb6a3b81915701
affanadeeb <134207565+affanadeeb@users.noreply.github.com>
2024-12-20 15:42:09 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   email: {
     type: String,
@@ -13,9 +15,24 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
+  },
+  profilePic: {
+    type: String,
+    default: ''
   }
-}, {
-  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model('User', userSchema); 

24a5dcdc4144b7015c3f05d317ef813c9517a89f
affanadeeb <134207565+affanadeeb@users.noreply.github.com>
2024-12-20 15:41:47 +0530

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -3,15 +3,40 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   content: {
     type: String,
-    required: true
+    required: function() {
+      return !this.mediaUrl;
+    }
   },
-  author: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
-  }
-}, {
-  timestamps: true
+  },
+  mediaUrl: {
+    type: String
+  },
+  feeling: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  reactions: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }]
 });
 
 module.exports = mongoose.model('Post', postSchema); 

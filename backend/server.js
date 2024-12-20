a5993f2146407185eb2384eb48495a04c8868257
affanadeeb <134207565+affanadeeb@users.noreply.github.com>
2024-12-20 10:43:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://connectify-xyz123.vercel.app', 'http://localhost:3000']
+    ? ['https://connectify-phi-rust.vercel.app', 'http://localhost:3000']
     : 'http://localhost:3000',
   credentials: true
 }));
